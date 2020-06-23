71c8acbea0a05d742e8669c1ab45131dd8bb4065
Anson008 <xczhao007@gmail.com>
2020-06-23 11:17:57 -0500

    Implement "reverse" that reverses a given sequence

---
+++ Chapter_4/Examples_C4/reverse.py
@@ -0,0 +1,12 @@
+def reverse(s, start, stop):
+    """Reverse elements in implicit slice s[start:stop]."""
+    if start < stop - 1:
+        s[start], s[stop - 1] = s[stop - 1], s[start]
+        reverse(s, start + 1, stop - 1)
+
+
+if __name__ == "__main__":
+    l1 = [1, 2, 3, 4]
+    reverse(l1, 0, 4)
+    print(l1)
+    
