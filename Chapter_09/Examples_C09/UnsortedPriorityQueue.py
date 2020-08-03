35685e3f627280373a535899669ba0fb52a72ae6
Anson008 <xczhao007@gmail.com>
2020-08-03 13:31:02 -0500

    Create UnsortedPriorityQueue.py

---
+++ Chapter_09/Examples_C09/UnsortedPriorityQueue.py
@@ -0,0 +1,9 @@
+from Chapter_09.Examples_C09.PriorityQueueBase import PriorityQueueBase
+
+
+class UnsortedPriorityQueue(PriorityQueueBase):
+    """A min-oriented priority queue implemented with an unsorted list."""
+
+    def _find_min(self):
+        """Return Position of item with minmum key."""
+        pass
